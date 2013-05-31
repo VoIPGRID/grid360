1c4ed02e6991e5d8ba6e16f768647195bd73b0d0
Ednan Pasagic <ednanpasagic@gmail.com>
2013-05-31 09:45:46 +0200

    Added LightOpenID and SwiftMailer libraries. Also updated SQL file and remove jQuery Numeric (switched to HTML5 inputs).

--- configs/grid360.sql
+++ configs/grid360.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 03, 2013 at 09:20 AM
+-- Generation Time: May 31, 2013 at 09:24 AM
 -- Server version: 5.5.29
 -- PHP Version: 5.4.6-1ubuntu1.2
 
@@ -31,7 +31,9 @@ CREATE TABLE IF NOT EXISTS `competency` (
   `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `description` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `competencygroup_id` int(11) unsigned DEFAULT NULL,
+  `tenant_id` int(11) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
+  KEY `index_foreignkey_competency_tenant` (`tenant_id`),
   KEY `index_foreignkey_competency_competencygroup` (`competencygroup_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=37 ;
 
@@ -45,7 +47,10 @@ CREATE TABLE IF NOT EXISTS `competencygroup` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `description` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `general` tinyint(3) unsigned DEFAULT NULL,
+  `tenant_id` int(11) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_foreignkey_competencygroup_tenant` (`tenant_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=6 ;
 
 -- --------------------------------------------------------
@@ -57,10 +62,14 @@ CREATE TABLE IF NOT EXISTS `competencygroup` (
 CREATE TABLE IF NOT EXISTS `department` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `tenant_id` int(11) unsigned DEFAULT NULL,
   `user_id` int(11) unsigned DEFAULT NULL,
+  `user` tinyint(1) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
+  KEY `index_foreignkey_department_tenant` (`tenant_id`),
   KEY `index_foreignkey_department_user` (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=7 ;
 
 -- --------------------------------------------------------
 
@@ -88,13 +97,15 @@ CREATE TABLE IF NOT EXISTS `review` (
   `competency_id` int(11) unsigned DEFAULT NULL,
   `rating_id` int(11) unsigned DEFAULT NULL,
   `round_id` int(11) unsigned DEFAULT NULL,
+  `tenant_id` int(11) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `index_foreignkey_review_user` (`reviewer_id`),
   KEY `cons_fk_review_reviewee_id_id` (`reviewee_id`),
   KEY `index_foreignkey_review_competency` (`competency_id`),
   KEY `index_foreignkey_review_rating` (`rating_id`),
-  KEY `index_foreignkey_review_round` (`round_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=114 ;
+  KEY `index_foreignkey_review_round` (`round_id`),
+  KEY `index_foreignkey_review_tenant` (`tenant_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=301 ;
 
 -- --------------------------------------------------------
 
@@ -108,10 +119,12 @@ CREATE TABLE IF NOT EXISTS `role` (
   `description` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `department_id` int(11) unsigned DEFAULT NULL,
   `competencygroup_id` int(11) unsigned DEFAULT NULL,
+  `tenant_id` int(11) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `index_foreignkey_role_competencygroup` (`competencygroup_id`),
+  KEY `index_foreignkey_role_tenant` (`tenant_id`),
   KEY `index_foreignkey_role_department` (`department_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=9 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=8 ;
 
 -- --------------------------------------------------------
 
@@ -123,8 +136,11 @@ CREATE TABLE IF NOT EXISTS `round` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `description` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `status` tinyint(3) unsigned DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=5 ;
+  `created` datetime DEFAULT NULL,
+  `tenant_id` int(11) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_foreignkey_round_tenant` (`tenant_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=6 ;
 
 -- --------------------------------------------------------
 
@@ -134,16 +150,30 @@ CREATE TABLE IF NOT EXISTS `round` (
 
 CREATE TABLE IF NOT EXISTS `roundinfo` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `status` tinyint(3) unsigned DEFAULT NULL,
+  `status` tinyint(1) unsigned DEFAULT NULL,
   `answer` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `round_id` int(11) unsigned DEFAULT NULL,
   `reviewer_id` int(11) unsigned DEFAULT NULL,
   `reviewee_id` int(11) unsigned DEFAULT NULL,
+  `round_id` int(11) unsigned DEFAULT NULL,
+  `tenant_id` int(11) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `index_foreignkey_roundinfo_user` (`reviewer_id`),
   KEY `cons_fk_roundinfo_reviewee_id_id` (`reviewee_id`),
-  KEY `index_foreignkey_roundinfo_round` (`round_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=26 ;
+  KEY `index_foreignkey_roundinfo_round` (`round_id`),
+  KEY `index_foreignkey_roundinfo_tenant` (`tenant_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=314 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tenant`
+--
+
+CREATE TABLE IF NOT EXISTS `tenant` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
 
 -- --------------------------------------------------------
 
@@ -156,16 +186,20 @@ CREATE TABLE IF NOT EXISTS `user` (
   `firstname` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `lastname` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `tmp_password` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `password` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `status` tinyint(3) unsigned DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
   `department_id` int(11) unsigned DEFAULT NULL,
   `role_id` int(11) unsigned DEFAULT NULL,
   `userlevel_id` int(11) unsigned DEFAULT NULL,
+  `tenant_id` int(11) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `index_foreignkey_user_department` (`department_id`),
   KEY `index_foreignkey_user_role` (`role_id`),
-  KEY `index_foreignkey_user_userlevel` (`userlevel_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=6 ;
+  KEY `index_foreignkey_user_userlevel` (`userlevel_id`),
+  KEY `index_foreignkey_user_tenant` (`tenant_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=32 ;
 
 -- --------------------------------------------------------
 
@@ -176,6 +210,7 @@ CREATE TABLE IF NOT EXISTS `user` (
 CREATE TABLE IF NOT EXISTS `userlevel` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `level` tinyint(3) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
 
@@ -187,18 +222,27 @@ CREATE TABLE IF NOT EXISTS `userlevel` (
 -- Constraints for table `competency`
 --
 ALTER TABLE `competency`
-  ADD CONSTRAINT `cons_fk_competency_competencygroup_id_id` FOREIGN KEY (`competencygroup_id`) REFERENCES `competencygroup` (`id`) ON DELETE SET NULL ON UPDATE SET NULL;
+  ADD CONSTRAINT `cons_fk_competency_competencygroup_id_id` FOREIGN KEY (`competencygroup_id`) REFERENCES `competencygroup` (`id`) ON DELETE SET NULL ON UPDATE SET NULL,
+  ADD CONSTRAINT `cons_fk_competency_tenant_id_id` FOREIGN KEY (`tenant_id`) REFERENCES `tenant` (`id`) ON DELETE SET NULL ON UPDATE SET NULL;
+
+--
+-- Constraints for table `competencygroup`
+--
+ALTER TABLE `competencygroup`
+  ADD CONSTRAINT `cons_fk_competencygroup_tenant_id_id` FOREIGN KEY (`tenant_id`) REFERENCES `tenant` (`id`) ON DELETE SET NULL ON UPDATE SET NULL;
 
 --
 -- Constraints for table `department`
 --
 ALTER TABLE `department`
+  ADD CONSTRAINT `cons_fk_department_tenant_id_id` FOREIGN KEY (`tenant_id`) REFERENCES `tenant` (`id`) ON DELETE SET NULL ON UPDATE SET NULL,
   ADD CONSTRAINT `cons_fk_department_user_id_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE SET NULL ON UPDATE SET NULL;
 
 --
 -- Constraints for table `review`
 --
 ALTER TABLE `review`
+  ADD CONSTRAINT `cons_fk_review_tenant_id_id` FOREIGN KEY (`tenant_id`) REFERENCES `tenant` (`id`) ON DELETE SET NULL ON UPDATE SET NULL,
   ADD CONSTRAINT `cons_fk_review_competency_id_id` FOREIGN KEY (`competency_id`) REFERENCES `competency` (`id`) ON DELETE SET NULL ON UPDATE SET NULL,
   ADD CONSTRAINT `cons_fk_review_rating_id_id` FOREIGN KEY (`rating_id`) REFERENCES `rating` (`id`) ON DELETE SET NULL ON UPDATE SET NULL,
   ADD CONSTRAINT `cons_fk_review_reviewee_id_id` FOREIGN KEY (`reviewee_id`) REFERENCES `user` (`id`) ON DELETE SET NULL ON UPDATE SET NULL,
@@ -209,13 +253,21 @@ ALTER TABLE `review`
 -- Constraints for table `role`
 --
 ALTER TABLE `role`
+  ADD CONSTRAINT `cons_fk_role_department_id_id` FOREIGN KEY (`department_id`) REFERENCES `department` (`id`) ON DELETE SET NULL ON UPDATE SET NULL,
   ADD CONSTRAINT `cons_fk_role_competencygroup_id_id` FOREIGN KEY (`competencygroup_id`) REFERENCES `competencygroup` (`id`) ON DELETE SET NULL ON UPDATE SET NULL,
-  ADD CONSTRAINT `cons_fk_role_department_id_id` FOREIGN KEY (`department_id`) REFERENCES `department` (`id`) ON DELETE SET NULL ON UPDATE SET NULL;
+  ADD CONSTRAINT `cons_fk_role_tenant_id_id` FOREIGN KEY (`tenant_id`) REFERENCES `tenant` (`id`) ON DELETE SET NULL ON UPDATE SET NULL;
+
+--
+-- Constraints for table `round`
+--
+ALTER TABLE `round`
+  ADD CONSTRAINT `cons_fk_round_tenant_id_id` FOREIGN KEY (`tenant_id`) REFERENCES `tenant` (`id`) ON DELETE SET NULL ON UPDATE SET NULL;
 
 --
 -- Constraints for table `roundinfo`
 --
 ALTER TABLE `roundinfo`
+  ADD CONSTRAINT `cons_fk_roundinfo_tenant_id_id` FOREIGN KEY (`tenant_id`) REFERENCES `tenant` (`id`) ON DELETE SET NULL ON UPDATE SET NULL,
   ADD CONSTRAINT `cons_fk_roundinfo_reviewee_id_id` FOREIGN KEY (`reviewee_id`) REFERENCES `user` (`id`) ON DELETE SET NULL ON UPDATE SET NULL,
   ADD CONSTRAINT `cons_fk_roundinfo_reviewer_id_id` FOREIGN KEY (`reviewer_id`) REFERENCES `user` (`id`) ON DELETE SET NULL ON UPDATE SET NULL,
   ADD CONSTRAINT `cons_fk_roundinfo_round_id_id` FOREIGN KEY (`round_id`) REFERENCES `round` (`id`) ON DELETE SET NULL ON UPDATE SET NULL;
@@ -224,6 +276,7 @@ ALTER TABLE `roundinfo`
 -- Constraints for table `user`
 --
 ALTER TABLE `user`
+  ADD CONSTRAINT `cons_fk_user_tenant_id_id` FOREIGN KEY (`tenant_id`) REFERENCES `tenant` (`id`) ON DELETE SET NULL ON UPDATE SET NULL,
   ADD CONSTRAINT `cons_fk_user_department_id_id` FOREIGN KEY (`department_id`) REFERENCES `department` (`id`) ON DELETE SET NULL ON UPDATE SET NULL,
   ADD CONSTRAINT `cons_fk_user_role_id_id` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`) ON DELETE SET NULL ON UPDATE SET NULL,
   ADD CONSTRAINT `cons_fk_user_userlevel_id_id` FOREIGN KEY (`userlevel_id`) REFERENCES `userlevel` (`id`) ON DELETE SET NULL ON UPDATE SET NULL;
